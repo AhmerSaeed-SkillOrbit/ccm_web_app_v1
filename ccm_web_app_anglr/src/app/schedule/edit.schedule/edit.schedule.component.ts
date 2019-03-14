e7167daf0164a2b534a8eeaaaebefb2a114eeda4
Syed Tabish Akhter <tabish.akhter@skillorbit.com>
2019-03-14 20:45:33 +0500

    commit

--- ccm_web_app_anglr/src/app/schedule/edit.schedule/edit.schedule.component.ts
+++ ccm_web_app_anglr/src/app/schedule/edit.schedule/edit.schedule.component.ts
@@ -559,8 +559,12 @@ export class EditScheduleComponent implements OnInit {
                     // this.schedule.scheduleDetails[index].endTime = null;
                 }
                 else {
-                    this.schedule.scheduleDetails[index].noOfShift = this.noOfShift;
-                    this.schedule.scheduleDetails[index].scheduleShifts = this.setTimeAllData;
+                    // this.schedule.scheduleDetails[index].noOfShift = this.noOfShift;
+                    // this.schedule.scheduleDetails[index].scheduleShifts = this.setTimeAllData;
+
+                    this.schedule.scheduleDetails[index].noOfShift = this._utilityService.deepCopy(this.noOfShift);
+                    this.schedule.scheduleDetails[index].scheduleShifts = this._utilityService.deepCopy(this.setTimeAllData);
+
                     // this.schedule.scheduleDetails[index].startTime = this.startTime;
                     // this.schedule.scheduleDetails[index].endTime = this.endTime;
                 }
@@ -574,7 +578,8 @@ export class EditScheduleComponent implements OnInit {
                         this.schedule.scheduleDetails[index].scheduleShifts = [];
                     }
                     else {
-                        this.schedule.scheduleDetails[index].scheduleShifts = this.setTimeAllData;
+                        // this.schedule.scheduleDetails[index].scheduleShifts = this.setTimeAllData;
+                        this.schedule.scheduleDetails[index].scheduleShifts = this._utilityService.deepCopy(this.setTimeAllData);
                     }
 
                 });
